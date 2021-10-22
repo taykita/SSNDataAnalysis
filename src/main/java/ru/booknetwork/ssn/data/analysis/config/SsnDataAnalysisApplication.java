0b42fdf2f515ca59d44c46283d89c8036bd6f6f9
taykita <artem.korovkin34@gmail.com>
2021-10-22 16:31:32 +0300

    fix

--- src/main/java/ru/booknetwork/ssn/data/analysis/config/SsnDataAnalysisApplication.java
+++ src/main/java/ru/booknetwork/ssn/data/analysis/config/SsnDataAnalysisApplication.java
@@ -2,6 +2,9 @@ package ru.booknetwork.ssn.data.analysis.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,20 +12,29 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = "ru.booknetwork.ssn.data.analysis")
 public class SsnDataAnalysisApplication {
-	@Autowired
-	public SsnDataAnalysisApplication(HibernateUtil hibernateUtil) {
-		this.hibernateUtil = hibernateUtil;
-	}
-
-	private final HibernateUtil hibernateUtil;
-
 	public static void main(String[] args) {
 		SpringApplication.run(SsnDataAnalysisApplication.class, args);
 	}
 
 	@Bean
 	public SessionFactory sessionFactory() {
-		return hibernateUtil.getSessionFactory();
+		return getSessionFactory();
+	}
+
+	private SessionFactory getSessionFactory() {
+		final StandardServiceRegistry registry = buildRegistry();
+		try {
+			return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+		} catch (Exception e) {
+			StandardServiceRegistryBuilder.destroy(registry);
+			throw new RuntimeException(e);
+		}
+	}
+
+	private StandardServiceRegistry buildRegistry() {
+		return new StandardServiceRegistryBuilder()
+				.configure()
+				.build();
 	}
 
 	@Bean
